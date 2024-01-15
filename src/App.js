94d1775e7118314f92ad73782622a55b2f16cf64
Ry-F3 <rhysfeew@gmail.com>
2024-01-15 18:42:47 +0000

    Add Navbar and fontawesome link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import NavBar from "./components/NavBar";
 import './App.css';
 import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <NavBar />
     </div>
   );
 }
