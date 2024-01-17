40e6bc12b0b8968fdb54b7fac9bf807dc4265c28
Ry-F3 <rhysfeew@gmail.com>
2024-01-17 20:07:20 +0000

    Add styles module folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import NavBar from "./components/NavBar";
-import './App.css';
+import styles from './App.module.css';
 import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
     </div>
   );
