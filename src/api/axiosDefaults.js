10cea97f094ae814490db1994fd33d2440780d17
Ry-F3 <rhysfeew@gmail.com>
2024-02-22 14:20:42 +0000

    Added icons to currentUser NavBar

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,5 +4,5 @@ axios.defaults.baseURL = 'https://django-rest-demo-1cfde67fddd7.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
-// export const axiosReq = axios.create();
-// export const axiosRes = axios.create();
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
